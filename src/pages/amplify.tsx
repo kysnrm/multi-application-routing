5d4bcdb935b6b76c4a6888538de54cdbca22ca0c
kysnrm <kysn.rm@gmail.com>
2023-08-08 20:02:47 +0900

    検証用ページを作成

--- src/pages/amplify.tsx
+++ src/pages/amplify.tsx
@@ -2,22 +2,20 @@ import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
+  const pageName = "Multi application routing | Amplify";
   return (
     <>
       <Head>
-        <title>Multi application routing</title>
+        <title>{pageName}</title>
       </Head>
       <main>
-        <h1>Multi application routing</h1>
+        <h1>{pageName}</h1>
         <section>
-          <h2>これは何？</h2>
           <p>
-            MPAからNext.jsへ段階的に移行していくにあたり、アプリケーション間のページ遷移が上手くいくのかを検証するためのアプリケーション。
+            このページはAmplifyにホストされたNext.jsアプリケーション内のページです。
           </p>
           <p>
-            <b>/amplify</b>
-            はAmplifyにホストされたNext.js内のページ、<b>/s3</b>
-            はs3に配置したhtmlファイルにそれぞれルーティングする想定。
+            <Link href="/">トップに戻る</Link>
           </p>
         </section>
         <nav>
